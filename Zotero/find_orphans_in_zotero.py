5838b2a0467502823e5274c670360de7e8fca495
cike <14360436+ci-ke@users.noreply.github.com>
2024-03-27 15:02:21 +0800

    Update find_orphans_in_zotero.py

--- Zotero/find_orphans_in_zotero.py
+++ Zotero/find_orphans_in_zotero.py
@@ -5,27 +5,26 @@ ZOTERO_STORAGE_FILES = r"D:\Workspace\OneDrive\vardtk\OneDrive\document\文献"
 import sqlite3
 import os
 
-dbh = sqlite3.connect(ZOTERO_STORAGE + "/zotero.sqlite")
-
 # Query all attachments
+dbh = sqlite3.connect(ZOTERO_STORAGE + "/zotero.sqlite")
 c = dbh.cursor()
-c.execute(
-    'select path from itemAttachments where contentType = "application/pdf" and linkMode = 2'
-)
+c.execute('select path from itemAttachments where linkMode = 2')
+# 'select path from itemAttachments where contentType = "application/pdf" and linkMode = 2'
 
-# Fetching all dirs as a set
-files = dict(
-    zip(
-        [f.lower() for f in os.listdir(ZOTERO_STORAGE_FILES)],
-        os.listdir(ZOTERO_STORAGE_FILES),
-    )
-)
+# Fetching all files as a set
+files = set()
+for dirpath, dirnames, filenames in os.walk(ZOTERO_STORAGE_FILES):
+    for f in filenames:
+        files.add(os.path.normpath(os.path.normcase(os.path.join(dirpath, f))))
 
 for key in c.fetchall():
+    # ('attachments:aaa/bbb.ccc',)
     filename = key[0].split(':')[1].lower()
-    if filename in files:
-        del files[filename]
+    filepath = os.path.normpath(
+        os.path.normcase(os.path.join(ZOTERO_STORAGE_FILES, filename))
+    )
+    files.discard(filepath)
 
 # Loop over the non-existing files
-orphans = sorted(files.values())
+orphans = sorted(files)
 print("\n".join(orphans))
